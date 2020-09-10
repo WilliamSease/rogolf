ad01e2470910b094e4b0c35650cac3f4034df380
Matthew Swanson <swansonmp@appstate.edu>
2020-09-10 01:30:58 -0400

    Add Bag and Club implementation

---
+++ Assets/Scripts/Bag.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bag
+{
+    private Game game;
+
+    private List<Club> bagList;
+    private int current;
+
+    public Bag(Game game)
+    {
+        this.game = game;
+        this.bagList = new List<Club>();
+
+        // Add default clubs
+        // name, power, shot loft (radians)
+        this.bagList.Add(new Club("1W", 10, 0.25));
+        this.bagList.Add(new Club("5I", 10, 0.6));
+        this.bagList.Add(new Club("SW", 10, 1));
+        this.bagList.Add(new Club("P", 10, 0));
+
+        this.current = bagList.Count - 1;
+    }
+
+    public void IncrementBag()
+    {
+        current++;
+        if (current >= bagList.Count)
+        {
+            current = 0;
+        }
+    }
+
+    public void DecrementBag()
+    {
+        current--;
+        if (current < 0)
+        {
+            current = bagList.Count - 1;
+        }
+    }
+
+    public Club GetClub() { return bagList[current]; }
+}
---
+++ Assets/Scripts/Club.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Club
+{
+    private string name;
+    private double power;
+    private double loft;
+
+    public Club(string name, double power, double loft)
+    {
+        this.name = name;
+        this.power = power;
+        this.loft = loft;
+    }
+
+    public string GetName() { return name; }
+    public double GetPower() { return power; }
+    public double GetLoft() { return loft; }
+
+    public void SetPower(double power) { this.power = power; }
+    public void SetLoft(double loft) { this.loft = loft; }
+}
