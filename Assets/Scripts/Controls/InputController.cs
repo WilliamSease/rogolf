5f7b47bd08855bd65269457e9cc548f8cdc74248
Matthew Swanson <swansonmp@appstate.edu>
2020-09-25 18:43:02 -0400

    Single delay 'hold down' for input

--- Assets/Scripts/Controls/InputController.cs
+++ Assets/Scripts/Controls/InputController.cs
@@ -1,72 +1,71 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class InputController
 {
     private Game game;
 	public Canvas console;
 
+    private const float ZERO = 0f;
+    private const float DELAY = 0.2f;
+    private List<Tuple<KeyCode, Action>> keyboard;
+    private float[] pressTimes;
+
     public InputController(Game game)
     {
         this.game = game;
 		console = (GameObject.Find("DevConsole")).GetComponent<Canvas>();
+
+        keyboard = new List<Tuple<KeyCode, Action>>();
+        keyboard.Add(new Tuple<KeyCode, Action>(KeyCode.Space, delegate() { game.state.OnKeySpace(); }));
+        keyboard.Add(new Tuple<KeyCode, Action>(KeyCode.UpArrow, delegate() { game.state.OnKeyUpArrow(); }));
+        keyboard.Add(new Tuple<KeyCode, Action>(KeyCode.DownArrow, delegate() { game.state.OnKeyDownArrow(); }));
+        keyboard.Add(new Tuple<KeyCode, Action>(KeyCode.RightArrow, delegate() { game.state.OnKeyRightArrow(); }));
+        keyboard.Add(new Tuple<KeyCode, Action>(KeyCode.LeftArrow, delegate() { game.state.OnKeyLeftArrow(); }));
+        keyboard.Add(new Tuple<KeyCode, Action>(KeyCode.W, delegate() { game.state.OnKeyW(); }));
+        keyboard.Add(new Tuple<KeyCode, Action>(KeyCode.A, delegate() { game.state.OnKeyA(); }));
+        keyboard.Add(new Tuple<KeyCode, Action>(KeyCode.S, delegate() { game.state.OnKeyS(); }));
+        keyboard.Add(new Tuple<KeyCode, Action>(KeyCode.D, delegate() { game.state.OnKeyD(); }));
+        keyboard.Add(new Tuple<KeyCode, Action>(KeyCode.Q, delegate() { game.state.OnKeyQ(); }));
+        keyboard.Add(new Tuple<KeyCode, Action>(KeyCode.E, delegate() { game.state.OnKeyE(); }));
+        keyboard.Add(new Tuple<KeyCode, Action>(KeyCode.Return, delegate() { game.state.OnKeyReturn(); }));
+
+        pressTimes = new float[keyboard.Count];
     }
 
     /// <summary>
     /// Check each relevant key.
-    /// If it is being pressed, propogate key press as method call to game's state.
-    /// 
-    /// This is hard coded right now! Deal with it!
+    /// If it is being pressed, propagate key press as method call to game's state.
     /// </summary>
     public void Tick()
     {
+        // Ignore keystrokes if console is active
 		if (console.enabled) return;
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        // Else process keys
+        float deltaTime = Time.deltaTime;
+        for (int i = 0; i < keyboard.Count; i++) ProcessKey(i, deltaTime);
+    }
+
+    private void ProcessKey(int i, float deltaTime)
+    {
+        KeyCode keyCode = keyboard[i].Item1;
+        if (Input.GetKey(keyCode))
         {
-            game.state.OnKeySpace();
+            UnityEngine.Debug.Log(keyCode);
+            pressTimes[i] -= deltaTime;
+            if (pressTimes[i] <= ZERO)
+            {
+                Action onKeyPress = keyboard[i].Item2;
+                onKeyPress();
+                pressTimes[i] += DELAY;
+            }
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyUp(keyCode))
         {
-            game.state.OnKeyUpArrow();
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            game.state.OnKeyDownArrow();
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            game.state.OnKeyRightArrow();
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            game.state.OnKeyLeftArrow();
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            game.state.OnKeyW();
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            game.state.OnKeyA();
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            game.state.OnKeyS();
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            game.state.OnKeyD();
-        }
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            game.state.OnKeyQ();
-        }
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            game.state.OnKeyE();
-        }
-        if (Input.GetKeyDown(KeyCode.Return)) // Enter key
-        {
-            game.state.OnKeyReturn();
+            pressTimes[i] = ZERO;
         }
     }
 }
