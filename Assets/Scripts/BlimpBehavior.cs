8a2af072f192ea726a3c55469679fc38e841f594
WilliamASease <seasewa@appstate.edu>
2020-10-29 16:37:38 -0400

    BLIMP confirmed

---
+++ Assets/Scripts/BlimpBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlimpBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject thisBlimp;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        thisBlimp.transform.Rotate(Vector3.up * Time.deltaTime * 2f);
+        thisBlimp.transform.Translate(Vector3.right * Time.deltaTime * 5f);
+    }
+}
